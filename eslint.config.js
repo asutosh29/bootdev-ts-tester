9d8cca8e967941522d9cc2b5d7f34a863353c62d
asutosh29 <asutosh29aaz@gmail.com>
2026-02-28 16:59:49 +0530

    feat: setup eslint

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+// eslint.config.mjs
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.ts", "**/*.tsx"], // Target TypeScript files
+    rules: {
+      // Add or override specific rules here
+      // For example, to warn about unused variables:
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
+  {
+    // Configuration for all files to ignore common build artifacts
+    ignores: [".next/**", "out/**", "build/**", "dist/**", "node_modules/**"],
+  },
+);
