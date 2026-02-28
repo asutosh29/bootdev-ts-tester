d17aef1934a144400d4e724776f81fbb38914ce2
asutosh29 <asutosh29aaz@gmail.com>
2026-02-28 16:24:18 +0530

    feat: Add working testing code

---
+++ src/main_test.ts
@@ -0,0 +1,56 @@
+import { describe, it, assert, withSubmit } from "./unit_test/utils";
+import { averageScore } from "./unit_test/utils";
+
+// type RunCase = {
+//   ratings: number[];
+//   expected: number;
+// };
+describe("averageScore", () => {
+  const runCases = [
+    {
+      ratings: [7, 11, 42],
+      expected: 20,
+    },
+    {
+      ratings: [17, 76, 17, 87, 18, 61],
+      expected: 46,
+    },
+  ];
+  const submitCases = runCases.concat([
+    {
+      ratings: [],
+      expected: 0,
+    },
+    {
+      ratings: [1111, 1337, 80085],
+      expected: 27511,
+    },
+  ]);
+
+  let testCases = runCases;
+  if (withSubmit) {
+    testCases = submitCases;
+  }
+
+  for (let i = 0; i < testCases.length; i++) {
+    // Prevent test case from being undefined
+    const testCase = testCases[i];
+    if (!testCase) {
+      continue;
+    }
+    const { ratings, expected } = testCase;
+    it(`Test ${i}`, () => {
+      const actual = averageScore(ratings);
+      console.log("Ratings: ", ratings);
+      console.log("Expected: ", expected);
+      console.log("Actual:   ", actual);
+      assert.strictEqual(actual, expected);
+    });
+    console.log("---------------------------------");
+  }
+
+  const numSkipped = submitCases.length - testCases.length;
+  if (numSkipped > 0) {
+    console.log(`- Skip: ${numSkipped} test case(s) for submit`);
+  }
+});
---
+++ src/unit_test/utils.ts
@@ -0,0 +1,27 @@
+import assert from "assert";
+
+// Toggle this to true to run the "submitCases" instead of just "runCases"
+export const withSubmit = false;
+export { assert };
+
+export function describe(description: string, callback: any) {
+  console.log(`\n--- ${description} ---`);
+  callback();
+}
+
+export function it(description: string, callback: any) {
+  try {
+    callback();
+    console.log(`✅ ${description}: Passed\n`);
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(`❌ ${description}: Failed`);
+      console.error(`   ${error.message}\n`);
+    }
+  }
+}
+
+// Functions
+export function averageScore(ratings: number[]) {
+  return 0;
+}
